a3de242837d5a1c9cdc5b2485c29b33a60ab981e
taku tongo <tongoonat@outlook.com>
2020-04-05 07:48:24 +0200

    add a custome user manager and added the migrations to the adatabase

--- src/profiles_projects/profile_api/models.py
+++ src/profiles_projects/profile_api/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """work with user custom user model """
+
+    def create_user(self, email, name, password=None):
+
+        if not email:
+            raise ValueError('user email must be provided')
+
+        email=self.normalize_email(email)
+        user= self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self,email,name,password):
+        """create a new user with the new given details"""
+        user = self.create_user(email,name,password)
+
+        user.is_superuser=True
+        user.is_stuff= True
+
+        user.save(using=self._db)
+
+class UserProfile(AbstractBaseUser,PermissionsMixin):
+    """Represents a user Profile inside our system"""
+
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    #object manager used to do other functions to our profile 
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """"used to get users full name"""
+
+        return self.name
+
+    def get_short_name(self):
+        """"used to get users short name """
+
+        return self.name
+
+    def __str__(self):
+        """convert object to string"""
+
+        return self.email
+    
